abb8ab81d6c5d31931faf3e0054dc14542afdb39
praveen9812 <54749786+praveen9812@users.noreply.github.com>
2021-03-29 00:46:43 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,16 @@
 <button class="sc" onclick="topFunction()" id="myBtn" title="Go to top">^</button>
-<div class="container">
+   <div class="container">
         <dev class="part search">   
              <input id="searchBar" class="searchbar" type="text" placeholder="Search in...">
-      <a id="btnSearch" class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></a>
+             <a id="btnSearch" class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></a>
         </dev>
+        
+        <div class="part logo"> 
+            <img src=" ////" alt="">
+        </div>
+           
+        <div class="part headericon">   
+            <i class="fa fa-user-circle" aria-hidden="true"></i>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </div>
