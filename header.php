e26b3126b6a63f83a41f03fec4cb77ec07ccd570
praveen9812 <54749786+praveen9812@users.noreply.github.com>
2021-03-29 00:44:11 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+<button class="sc" onclick="topFunction()" id="myBtn" title="Go to top">^</button>
+<div class="container">
+        <dev class="part search">   
+             <input id="searchBar" class="searchbar" type="text" placeholder="Search in...">
+      <a id="btnSearch" class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></a>
+        </dev>
